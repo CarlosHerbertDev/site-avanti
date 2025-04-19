71671216023c785f1d2801cd94641a2a100a23b8
Carlos Herbert <hrbrtvieira@gmail.com>
2025-04-18 23:49:21 -0300

    criando nova parte do menu

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,13 @@ const ulMegaMenu = document.querySelector(".slide-menu");
 const megaMenuContent = document.querySelector(".mega-menu-content");
 const todasCategorias = document.querySelector("#todas-categorias");
 const categories = document.querySelectorAll(".categories");
+const menuItem = document.querySelectorAll(".menu-item");
 const departamentos = [
   "Departamento", "Departamento", "Departamento", "Departamento",
   "Departamento", "Departamento", "Departamento", "Departamento",
-  "Departamento", "lucas", "Departamento", "Departamento"
+  "Departamento", "Departamento", "Departamento", "Departamento"
 ];
+
 
 let hideTimeout;
 
@@ -38,63 +40,45 @@ function mostrarMenu() {
       const category = document.querySelector(`[data-dept="${departamento}"]`)
 
       item.addEventListener("mouseenter", () => {
-        // Remover ativo dos outros
+ 
         itemMegaMenu.forEach((i) => i.classList.remove("active"));
         item.classList.add("active");
+        categories.forEach((c) => c.classList.remove("active"));
+        category.classList.add("active");
 
-        // Mudar seta
+      
         if (arrow) arrow.src = "./src/assets/arrow-hover.png";
         
-        category.classList.remove("hidden");
         
         if (category.dataset.dept === 'Departamento') {
           
+          console.log('ai');
           
         category.innerHTML = `
           <div class="container-categories">
             <ul class="categories-ul">
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+              <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
             </ul>
             <div class="container-image">
               <a>Imagem</a>
             </div>
           </div>
         `;
-        // const containerCategories = document.querySelector(".conatiner-categories");
-      } else if (category.dataset.dept === 'lucas') {
-        category.innerHTML = `
-          <div class="container-categories">
-            <ul class="categories-ul">
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-              <li><a>categoria</a> <a>categoria</a> <a>categoria</a></li>
-            </ul>
-            <div class="container-image">
-              <img src="./src/assets/arrow.png" alt="imagem">
-            </div>
-          </div>
-        `;
-        
-      }
+
+      } 
     })
     
     
     item.addEventListener("mouseleave", () => {
       if (arrow) arrow.src = "./src/assets/arrow.png";
-          category.classList.add("hidden");
-      })
+        })
     });
   }
 }
@@ -105,17 +89,16 @@ function esconderMenu() {
     ulMegaMenu.classList.add("hidden");
     megaMenuContent.classList.add("hidden");
     
-    // Resetar os estilos ativos
     todasCategorias.classList.remove("active");
     document.querySelectorAll(".mega-menu-item").forEach((item) => {
       item.classList.remove("active");
       const arrow = item.querySelector("img");
       if (arrow) arrow.src = "./src/assets/arrow.png";
+      categories.forEach((c) => c.innerHTML = "");
     });
   }, 200);
 }
 
-// Eventos
 
 const containerCategories = document.querySelectorAll(".container-categories");
 
@@ -124,7 +107,6 @@ todasCategorias.addEventListener("mouseenter", mostrarMenu);
 todasCategorias.addEventListener("mouseleave", (event) => {
   const related = event.relatedTarget;
 
-  // Se o mouse ainda estiver dentro do mega menu (ex: indo pra .categories), não esconde
   if (!megaMenu.contains(related)) {
     esconderMenu();
   }
@@ -134,17 +116,46 @@ megaMenu.addEventListener("mouseenter", () => clearTimeout(hideTimeout));
 megaMenu.addEventListener("mouseleave", (event) => {
   const related = event.relatedTarget;
 
-  // Se o mouse ainda estiver dentro do mega menu (inclusive categorias), não esconde
   if (!megaMenu.contains(related) && related !== todasCategorias) {
     esconderMenu();
   }
 });
 
 
-// categories.forEach((category) => {
-//   category.addEventListener("mouseenter", () => {
-//     console.log(category);
-//       category.classList.remove("hidden");
 
-//     })
-// })
+menuItem.forEach((item) => { 
+  
+  if (item.innerHTML === "<strong>☰ Todas as Categorias</strong>") return;
+  
+  item.addEventListener("mouseenter", () => {
+
+    const headerCategories = document.querySelector(`.header-categories[data-dept="${item.innerText}"]`);
+    console.log(headerCategories);
+    
+
+    headerCategories.innerHTML +=`
+      <h4>Departamento</h4>
+      <ul>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+          <li><a>Categoria</a> <a>Categoria</a> <a>Categoria</a></li>
+      </ul>
+    
+    `
+  });
+
+  item.addEventListener("mouseleave", () => {
+    console.log("oi");
+    
+    const headerCategories = document.querySelectorAll(".header-categories");
+    headerCategories.forEach((header) => {
+      header.innerHTML = "";
+    })
+  });
+
+})
