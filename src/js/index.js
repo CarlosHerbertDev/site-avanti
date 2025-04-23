62a67b78aea887d4272128765c4ec3ac1cde85ff
CarlosHerbertDev <hrbrtvieira@gmail.com>
2025-04-23 16:28:24 -0300

    criando corrossel

--- src/js/index.js
+++ src/js/index.js
@@ -26,7 +26,24 @@ const leftArrow = document.querySelector('.arrow.left');
 const rightArrow = document.querySelector('.arrow.right');
 const cards = document.querySelectorAll('.card');
 
-const cardWidth = cards[0].offsetWidth + 1246; // largura + gap
+
+
+for (let index = 0; index < 13; index++) {
+    carousel.innerHTML += `
+      <div class="card">
+            <span class="badge">NOVO</span>
+            <img src="https://via.placeholder.com/200x250" alt="Produto" />
+            <h3>Lorem Ipsum Dolor Sit Amet</h3>
+            <p class="old-price">R$ 100,00</p>
+            <span class="discount">10% OFF</span>
+            <p class="new-price">R$ 79,90</p>
+            <p class="installments">Ou em até 10x de R$ 7,90</p>
+            <button>Comprar</button>
+      </div>
+    `
+}
+
+const cardWidth = cards[0].offsetWidth + 986; // largura + gap
 const maxIndex = dots.length - 1;
 let currentIndex = 0;
 
@@ -51,4 +68,5 @@ rightArrow.addEventListener('click', () => {
   if (currentIndex < maxIndex) {
     updateCarousel(currentIndex + 1);
   }
+  
 });
